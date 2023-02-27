1d17c19bd21eb8b81ee5190306b90e7fd3c11006
B-Waterman <brooke.waterman@outlook.com>
2023-02-26 23:22:57 -0800

    comepleted request func & dependent fs.writeFile. Testing now WIP

--- fetcher.js
+++ fetcher.js
@@ -5,25 +5,24 @@
 //2)should download URL's resource to local file path given
 //3)should console.log msg like "Download complete and [#] bytes saved to [local file path]"
 
-//Make https request, wait for response
-//  -GET: summon thy resource from webpage
-//then write resource data to file
-
 
 const request = require('request');
 const fs = require('fs');
 
-const targetURL = process.argv[]; 
+const targetURL = process.argv[1]; 
 //the url meant to be downloaded, input on the cmd line, unknown what order yet
-const targetFilePath = process.argv[];
+const targetFilePath = process.argv[2];
 //the location the file will be copied to, unknown order rn
 
 request(targetURL, (err, response, body) => {
   if (err) console.error(err);
   fs.writeFile(targetFilePath, body, err => {
-    if (err) {
-      console.error(err);
-    }
+    if (err) console.error(err);
+
+    const fileStats = fs.statSync(targetFilePath);
+    const fileSize = fileStats.size;
+    console.log(`Download complete: ${fileSize} bytes saved to ${targetFilePath}`);
+
   });
 
 });
